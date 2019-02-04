b947e02ef77faf4a06e98743b94f1dedd87d57bd
Piotr Rynkowski <rynek0@onet.eu>
2019-02-04 16:34:30 +0100

    first look of timer

--- Whacamole/script.js
+++ Whacamole/script.js
@@ -3,3 +3,19 @@ const cats = document.querySelectorAll('.cat');
 const time = document.querySelector('.header__time');
 
 
+let distance = 60;
+let x = setInterval(function () {
+    distance = distance -1;
+    if (distance < 10) 
+    document.getElementById("header__time").innerHTML = distance ;
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("header__time").innerHTML = "Game over";
+
+        }
+}, 1000);
+
+function addMoreTime() {
+    distance += 5;
+}
+
