7afaa33cd33dc2296758d3b79405550f42885604
Mateusz Parol <matimuzykant@gmail.com>
2019-02-05 15:46:53 +0100

    something

--- Whacamole/script.js
+++ Whacamole/script.js
@@ -32,7 +32,7 @@ const randomCat = function () {
 
 };
 
-setInterval(randomCat, 5000);
+setInterval(randomCat, 1000);
 
 
 
@@ -43,25 +43,26 @@ let playbutton = document.querySelector(".playbutton");
 let underbutton = document.querySelector(".underbutton");
 let header =  document.querySelector(".header");
 let isOpen = false;
+let start = document.querySelector(".wrapper")
 
-function sezamie() {
+function sesame() {
   if (isOpen === false) {
     leftdoor.style.left = -50 + "vw";
     rightdoor.style.left = 100 + "vw";
     playbutton.style.left = 93.8 + "vw";
     underbutton.style.left = -6.5 + "vw";
     isOpen = true;
-    header.style.opacity = 2;
-  } else {
+    header.style.opacity = 2; }
+  else {
     leftdoor.style.left = 0 + "vw";
     rightdoor.style.left = 50 + "vw";
     playbutton.style.left = 43.5 + "vw";
     underbutton.style.left = 43.5 + "vw";
     isOpen = false;
     header.style.opacity = 0;
-  }
-}
+  } }
 
 
-playbutton.addEventListener("click", sezamie);
-underbutton.addEventListener("click", sezamie);
+
+playbutton.addEventListener("click", sesame);
+underbutton.addEventListener("click", sesame);
